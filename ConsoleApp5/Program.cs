b60db14da127389a30b6f6deb4370925c271246c
KenanNagiyev <788lnmp@code.edu.az>
2022-03-05 10:36:11 +0400

    new project

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metn = Console.ReadLine();
+            
+                for (int i =0; i<metn.Length;i++)
+            {
+                if (metn[i] == 'a')
+                {
+                    Console.WriteLine("metnde 'a' simvolu var");
+                    break;
+                }
+                if (metn[i] != 'a')
+                {
+                    Console.WriteLine("metnde 'a' simvolu yoxdur");
+                    break;
+                }
+            }
+            
+        }
+    }
+}
